c9e53b709ffd549812fa8f33800deaef44ff3e88
Charles Lombardo <clombardo169@gmail.com>
2024-03-10 22:13:24 -0400

    Adjust testing setup

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ mod config;
 use config::Config;
 
 fn main() -> Result<(), std::io::Error> {
-    let file_path = String::from("foo.txt");
+    let file_path = String::from("test/stuff/foo.txt");
     let mut config = Config::new(&file_path, config::LineEnding::LF, true);
-    config.save();
 
     let key = String::from("new-setting");
+    let other_key = String::from("a-setting");
     let category = String::from("new-category");
-    config.remove(&category, &key);
+    let my_category = String::from("a-category");
+    let value = 20;
+    config.add(&category, &key, &value);
+    config.add(&my_category, &key, &value);
+    config.add(&category, &other_key, &value);
 
     config.save();
 
