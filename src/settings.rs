86a84544750402c2137dc2f0bcf2f4080eaeae2a
Charles Lombardo <clombardo169@gmail.com>
2024-03-10 22:14:11 -0400

    Pass references to Setting data in getters

--- src/settings.rs
+++ src/settings.rs
@@ -7,45 +7,44 @@ pub struct Setting {
 }
 
 impl Setting {
-    pub fn new<T: std::fmt::Display>(key: String, category: String, value: T) -> Setting {
+    pub fn new<T: std::fmt::Display>(key: &String, category: &String, value: &T) -> Setting {
         let new_key: String = if key.contains("=") {
             let temp_key = key.clone();
             temp_key.replace("=", "")
         } else {
-            key
+            key.clone()
         };
 
         Setting {
             key: new_key,
-            category,
+            category: category.clone(),
             value: value.to_string(),
         }
     }
 
-    pub fn get_key(&self) -> String {
-        self.key.clone()
+    pub fn get_key(&self) -> &String {
+        &self.key
     }
 
-    pub fn get_category(&self) -> String {
-        self.category.clone()
+    pub fn get_category(&self) -> &String {
+        &self.category
     }
 
     pub fn set_value<T: std::fmt::Display>(&mut self, value: T) {
-        self.value = value.to_string();
-        self.value = self.value.trim().to_string();
+        self.value = value.to_string()
     }
 
-    pub fn get_value_string(&self) -> String {
-        self.value.clone()
+    pub fn get_value_string(&self) -> &String {
+        &self.value
     }
 
     pub fn get_value<T: std::str::FromStr>(&self, default_value: T) -> T {
-        self.value.parse::<T>().unwrap_or_else(|_| default_value)
+        self.value.parse::<T>().unwrap_or(default_value)
     }
 }
 
 impl std::fmt::Display for Setting {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Key - {}, Category - {}, Value - {}", self.key, self.category, self.value)
+        write!(f, "Category - {}, Key - {}, Value - {}", &self.key, &self.category, &self.value)
     }
 }
