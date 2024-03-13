bfa1d062bd702e5498a190dde76da3224c0eecda
Charles Lombardo <clombardo169@gmail.com>
2024-03-12 23:27:34 -0400

    Use a more robust test

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,31 @@ mod config;
 use config::Config;
 
 fn main() -> Result<(), std::io::Error> {
-    let file_path = String::from("test/stuff/foo.txt");
+    let file_path = String::from("test/foo.txt");
     let mut config = Config::new(&file_path, config::LineEnding::LF, true);
 
-    let key = String::from("new-setting");
-    let other_key = String::from("a-setting");
-    let category = String::from("new-category");
-    let my_category = String::from("a-category");
-    let value = 20;
-    config.add(&category, &key, &value);
-    config.add(&my_category, &key, &value);
-    config.add(&category, &other_key, &value);
+    let mut value: u128 = 20;
+    for i in 0..1000 {
+        let category = format!("this-category-{}", i);
+        for j in 0..1000 {
+            let key = format!("this-key-{}", j);
+            config.add(&category, &key, &value);
+            value += j;
+        }
+    }
+
+    for i in 0..1000 {
+        let category = format!("this-category-{}", i);
+        for j in 0..1000 {
+            let key = format!("this-key-{}", j);
+            let setting = config.get_mut(&category, &key).unwrap();
+            let value = setting.get_value(1u128) + 1;
+            setting.set_value(value);
+        }
+    }
 
     config.save();
+    config.reload();
 
     Ok(())
 }
